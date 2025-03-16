999c8adc66ddc2f4c13f4dd20470bc1d6cefe285
DianaLaura <dianalaura.enggist@gmx.ch>
2025-03-16 02:30:13 +0100

    Solved exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,19 +8,22 @@ const Header = (props) => {
   )
 }
 
+//Information about a specific part
+const Part = (props) => {
+  return(
+    <p>
+        {props.part} {props.exercises}
+    </p>
+  )
+}
+
 //course parts and their number of exercises
 const Content = (props) => {
   return(
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
